c3ed0bcd085d33a1f33fc492fb13d5c87ecb1918
KryvkoSergii <kryvkosergii@gmail.com>
2016-03-20 13:50:25 +0200

    Add new class in new Fink

--- test/src/main/java/SomeClass.java
+++ test/src/main/java/SomeClass.java
@@ -1,5 +1,16 @@
+import java.util.Date;
+
 /**
  * Created by srg on 3/20/16.
  */
 public class SomeClass {
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
